6503887a77078da85a5d17c0028888082ebefcd8
loteknowledG <loteknowledG@gmail.com>
2020-10-12 15:45:45 -0400

    Update Game.js

--- src/components/Matrix/Game/Game.js
+++ src/components/Matrix/Game/Game.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Card, CardActionArea, CardMedia, Grid } from '@material-ui/core'
 import { Cookie } from '../Cookie/Cookie'
 import { makeStyles } from '@material-ui/core/styles'
+import { motion } from 'framer-motion'
 import useGlobal from '../../../store'
 import { useHistory } from 'react-router-dom'
 
@@ -32,6 +33,7 @@ export function Game (props) {
   }
   const classes = useStyles();
   const [momentIdx, setMomentIdx] = useState(0)
+  const constraintsRef = useRef(null);
   let sat = 10
   let ppp = 0
   function handlePP(pp) {
@@ -49,10 +51,8 @@ export function Game (props) {
   }
   return (
     <Grid container justify="center" className={classes.grid} >
-      
+        <motion.div ref={constraintsRef} >
         <Card>
-          
-        
           <CardActionArea >
             <Cookie handlePP={(pp) => handlePP(pp)}> 
             <img alt="" 
@@ -60,14 +60,10 @@ export function Game (props) {
               src={globalState.level.pix[momentIdx].src}
             >              
             </img>
-            
-            
             </Cookie>
-          </CardActionArea>
-          
+          </CardActionArea>          
         </Card>
-       {/* <img alt="" className={classes.fullHeight} src="https://lh3.googleusercontent.com/Q2pE7BbwYPNn_jt9_Fng7jzkwxcbEAmk6wJ4EH8UgkKLKiSASIDumXk0Q1VwsZn89kABNnfuzN-M9xkgM9TvYwhPeEDqxHeZKAYP0T32jsZvAAFH3bqCH6_UkAAPNgVqA0frMRtXNfC1YyBzpPjfk2IMHFDJzq0rjLNLjt2wwCr50cK4pBiIuBsNt3ysvNskGItOQduspOIiu3ZW_pK_WhMGZaNylZfzvG_8GKYTrnHFiVCusmIu54NUwTT56Gk48Jpid7E3-6lDpNd2zQRSyh4NM8wiKvO6nsMDxThjrM2Poj1Vji4Q-JdzNlYaYwhXIwzx3zaDUfoRWSY_ZyQPoCpWcS_jSxjB-EEJru2aGxsUNYOmqYzjW0-AmUl3vQ2DvvehQV4flcY0DSQu34U8Fo2ATIGWAPmr1o_zxJA6KgxVKJeXJADQAXbJXi3Ii2JaCLJjI5eIHxKTW8KYyCEuTOEt7E4dEPmCCdSVEGqJC68CNDK405eqHWhF88onm0eObztW7MWbG62V99hITmowwHXI2DAm7cxJWnGZkIi1GZ26_IJ04wgkan1yvQPxnY4q2UnxcjBtAauGLs1LjwV6c1CWcgJIzE8M8bbZCFdLPzgcw4jnvNV9XKbrjjGqaIzzv8e0AI_q_on6as3RLmXu1tm2NhDctfdHuoJECqDM_zhoFDwIHcaSwebm_43bmDMl9DNLlo2cWep8QgiU6pAu4Gg50suMlz5nzg4jYniYil8bgktM6A"></img> */}
-      
+        </motion.div>      
     </Grid>
   )
 
