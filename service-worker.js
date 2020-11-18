5d1f5dfc2d27efbbba95f317af47ce246d451894
loteknowledG <loteknowledG@gmail.com>
2020-11-18 12:51:20 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noosphere/precache-manifest.f2dc75138c80620578cac806478cc56f.js"
+  "/noosphere/precache-manifest.2fce196f146f069bc5282173e7db954f.js"
 );
 
 self.addEventListener('message', (event) => {
